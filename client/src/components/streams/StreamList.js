72bcdf9369089388222dcdabd3f5caf9648a357b
SOHAG <SohagChowdhury321@gmail.com>
2019-07-30 02:30:15 +0600

    Linking to the stream creation

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link }  from 'react-router-dom';
 import { fetchStreams } from '../../actions'
 
 class StreamList extends React.Component{
 
     componentDidMount(){
         this.props.fetchStreams();      
+    }
+    renderCreate(){
+        if(this.props.userSignIn){
+            return(
+                <div style={{textAlign:"right"}}>
+                    <Link to="streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            )
+        }
     }
     renderAdmin(stream){
         if(stream.userId===this.props.currentUserId)
@@ -22,7 +32,7 @@ class StreamList extends React.Component{
         return this.props.streams.map((stream)=>{
            return (
                <div className="item" key={stream.id}>
-                     {this.renderAdmin(stream)}
+                     {this.renderAdmin(stream)}                  
                     <i className="large middle aligned icon camera"/>
                     <div className="content">
                         {stream.title}
@@ -43,6 +53,7 @@ class StreamList extends React.Component{
                 <div className="ui celled list">
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         )
     }
@@ -50,7 +61,8 @@ class StreamList extends React.Component{
 
 const mapStateToProps=(state)=>{
     return {streams:Object.values(state.streams),
-            currentUserId: state.auth.userId
+            currentUserId: state.auth.userId,
+            userSignIn: state.auth.isSignedIn
     }
 }
 
